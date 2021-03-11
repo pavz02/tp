1811d145c3ddc3981ee66b7367ce2a27bd586c50
pavitra <pavirajan02@gmail.com>
2021-03-11 08:32:58 +0800

    Changed the string MESSAGE_USAGE for a few commands.

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -12,9 +12,9 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all orders";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows a list of all persons from the address book.";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows a list of all orders from CakeCollate.";
 
 
     @Override
